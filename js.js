3cdd0f520198333c7bef46f4802ebfe4d3e9dc21
ihor-vip <vip20140902@gmail.com>
2021-07-12 16:02:13 +0300

    js

---
+++ js.js
@@ -0,0 +1,116 @@
+//let a = 'hello';
+//let b = 'owu';
+//let c = 'com';
+//let d = 'ua';
+//let e = 1;
+//let f = 10;
+//let g = -999;
+//let h = 123;
+//let i = '3.14';
+//let j = '2.7';
+//let k = '16';
+//let l = true;
+//let m = false;
+//console.log(a,b,c,d,e,f,g,h,i,j,k,l,m);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+// alert(m);
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(e);
+// document.write(f);
+// document.write(g);
+// document.write(h);
+// document.write(i);
+// document.write(k);
+// document.write(l);
+// document.write(m);
+//a=1;
+//b=2;
+//c=3;
+//d=4;
+//e=5;
+//f=6;
+//g=7;
+//h=8;
+//i=9;
+//j=10
+//k=11;
+//l=12;
+//m=13;
+//console.log(a,b,c,d,e,f,g,h,i,j,k,l,m);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(l);
+// alert(m);
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(e);
+// document.write(f);
+// document.write(g);
+// document.write(h);
+// document.write(i);
+// document.write(k);
+// document.write(l);
+// document.write(m);
+
+//let person = {
+//firstName: 'Ihor',
+// middleName: 'Ivanovich',
+// lastName: 'Ploskodnyak'}
+
+//console.log(person.firstName);
+//console.log(person.middleName);
+//console.log(person.lastName);
+
+//let a = prompt('enter your first name');
+//let b = prompt('enter your middle name');
+//let c = prompt('enter your age');
+//console.log(`congrats ${a} ${b}. you are ${c} years old`);
+
+// let a = 100;
+//console.log(typeof a)
+
+//let b = '100';
+//console.log(typeof b)
+
+//let c = true;
+//console.log(typeof c)
+
+//let truth = true;
+//truth = 5<6;
+//truth = 5>6;
+//truth = 5===6;
+//truth = 5==6;
+//truth = 10===10;
+//truth = 10>=10;
+//truth = 10>10;
+// truth = 10<10;
+//truth = 10!=10;
+//truth = 123==='123';
+//truth = 123 == '123';
+//console.log(truth)
+
